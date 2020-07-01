d27e15674d444d4f9af14b509b1b28c6b3ed2f70
wizaral <alex.macromaster@outlook.com>
2020-07-01 17:41:51 +0300

    add behavior to figure class

---
+++ logic/inc/figure.hpp
@@ -0,0 +1,29 @@
+#pragma once
+#include <memory>
+#include "strategy.hpp"
+
+namespace Chess::Logic {
+
+enum class FigureType {
+    Pawn, Knight, Bishop, Rook, Queen, King,
+};
+
+enum class FigureColor {
+    Light, Dark,
+};
+
+class Figure {
+    FigureType type_;
+    FigureColor color_;
+    std::unique_ptr<Strategy> st_;
+public:
+    Figure(FigureType type, FigureColor color, std::unique_ptr<Strategy> st)
+        : type_(type), color_(color), st_(std::move(st)) {}
+
+    Figure(Figure &&f)
+        : type_(f.type_), color_(f.color_), st_(std::move(f.st_)) {}
+
+    ~Figure() = default;
+};
+
+}; // namespace Chess::Logic
