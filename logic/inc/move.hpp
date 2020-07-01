40daeb1b121c0fbf93bc0a0c8cb2dbed1aa778d5
wizaral <alex.macromaster@outlook.com>
2020-07-01 17:40:06 +0300

    add move class

---
+++ logic/inc/move.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "position.hpp"
+#include <utility>
+
+namespace Chess::Logic {
+
+class Move {
+    Position from_, to_;
+public:
+    Move(const Position &from, const Position &to)
+        : from_(from), to_(to) {}
+
+    Position from() const {
+        return from_;
+    }
+
+    Position to() const {
+        return to_;
+    }
+};
+
+} // namespace Chess::Logic
