4388be910027989f150a554c4b479902a4f7882b
wizaral <alex.macromaster@outlook.com>
2020-07-02 10:56:44 +0300

    strategy interface and realizations

--- logic/inc/strategy.hpp
+++ logic/inc/strategy.hpp
@@ -1,13 +1,46 @@
 #pragma once
 #include <memory>
+
 #include "board.hpp"
 #include "figure.hpp"
 #include "move.hpp"
 
 namespace Chess::Logic {
 
-struct Strategy {
-    virtual void validate_move(std::unique_ptr<Figure> figure, const Board &board, const Move &move) = 0;
+class Strategy {
+public:
+    virtual ~Strategy() = default;
+    virtual void validate_move(std::unique_ptr<Figure> figure, const Board &board, const Move &move) const = 0;
+};
+
+class BishopStrategy : public Strategy {
+public:
+    void validate_move(std::unique_ptr<Figure> figure, const Board &board, const Move &move) const override;
+};
+
+class KingStrategy : public Strategy {
+public:
+    void validate_move(std::unique_ptr<Figure> figure, const Board &board, const Move &move) const override;
+};
+
+class KnightStrategy : public Strategy {
+public:
+    void validate_move(std::unique_ptr<Figure> figure, const Board &board, const Move &move) const override;
+};
+
+class PawnStrategy : public Strategy {
+public:
+    void validate_move(std::unique_ptr<Figure> figure, const Board &board, const Move &move) const override;
+};
+
+class RookStrategy : public Strategy {
+public:
+    void validate_move(std::unique_ptr<Figure> figure, const Board &board, const Move &move) const override;
+};
+
+class QueenStrategy : public BishopStrategy, public RookStrategy {
+public:
+    void validate_move(std::unique_ptr<Figure> figure, const Board &board, const Move &move) const override;
 };
 
 }; // namespace Chess::Logic
