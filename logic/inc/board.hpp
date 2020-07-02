93943a710684af6478cd233a2f7c518da6059f6b
wizaral <alex.macromaster@outlook.com>
2020-07-02 10:58:34 +0300

    add test map for check mechanics

--- logic/inc/board.hpp
+++ logic/inc/board.hpp
@@ -2,34 +2,43 @@
 #include "figure.hpp"
 #include <array>
 #include <memory>
+#include <utility>
 
 namespace Chess::Logic {
 
 class Board {
-    using Field = std::array<std::array<std::unique_ptr<Figure>, 8>, 8>;
+    template <class T>
+    using Field = std::array<std::array<T, 8>, 8>;
 
-    Field board;
+    Field<std::unique_ptr<Figure>> figures_;
+    Field<std::pair<bool, bool>> check_state_;
 
 public:
     Board() = default;
 
-    const Figure &get_figure(Position pos) const {
-        return *board[pos.row()][pos.col()].get();
+    Figure *get_figure(Position pos) const {
+        return figures_[pos.row()][pos.col()].get();
+    }
+
+    bool get_state(Position pos, FigureColor color) const {
+        if (color == FigureColor::Light)
+            return check_state_[pos.row()][pos.col()].first;
+        return check_state_[pos.row()][pos.col()].second;
     }
 
     void add_figure(Figure figure, Position pos) {
         Position::validation(pos);
-        board[pos.row()][pos.col()] = std::make_unique<Figure>(std::move(figure));
+        figures_[pos.row()][pos.col()] = std::make_unique<Figure>(std::move(figure));
     }
 
     void remove_figure(Position pos) {
         Position::validation(pos);
-        board[pos.row()][pos.col()].reset();
+        figures_[pos.row()][pos.col()].reset();
     }
 
     void move_figure(const Move &move) {
-        board[move.from().row()][move.from().col()] =
-            std::move(board[move.to().row()][move.to().col()]);
+        figures_[move.from().row()][move.from().col()] =
+            std::move(figures_[move.to().row()][move.to().col()]);
     }
 };
 
