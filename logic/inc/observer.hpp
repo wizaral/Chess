19db945822f8210f6b033e2e8c35d32d91c9dc9b
Oleksandr Afanasiev <oafanasiev@e1r3p5.unit.ua>
2020-07-04 21:07:27 +0300

    update dctors

--- logic/inc/observer.hpp
+++ logic/inc/observer.hpp
@@ -3,14 +3,14 @@
 
 class Observer {
 public:
-    virtual ~Observer() {}
+    virtual ~Observer() = default;
     virtual void update() = 0;
 };
 
 class Subject {
     std::list<Observer *> list;
 public:
-    virtual ~Subject() {}
+    virtual ~Subject() = default;
     virtual void subscribe(Observer *ob) = 0;
     virtual void unsubscribe(Observer *ob) = 0;
     virtual void notify() = 0;
