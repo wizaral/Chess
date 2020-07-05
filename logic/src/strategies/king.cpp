5d5df3d32e117007276e2cf521f015024bde6d95
wizaral <alex.macromaster@outlook.com>
2020-07-05 19:55:32 +0300

    king strategy implementation

--- logic/src/strategies/king.cpp
+++ logic/src/strategies/king.cpp
@@ -1,5 +1,3 @@
-#include <cmath>
-
 #include "board.hpp"
 #include "figure.hpp"
 #include "move.hpp"
@@ -7,13 +5,14 @@
 
 namespace Chess::Logic {
 
-bool KnightStrategy::validate_move(const Figure &figure, const Board &board, const Move &move) {
+bool KingStrategy::validate_move(const Figure &figure, const Board &board, const Move &move) {
     int rows = move.rows();
     int cols = move.cols();
 
-    if ((rows == 2 && cols == 1) || (rows == 1 && cols == 2)) {
+    if (rows == 1 || cols == 1) {
         Figure *other = board.get_figure(move.to());
         if (other == nullptr || other->color() != figure.color()) {
+            state_ = MoveState::NormalMove;
             return true;
         }
     }
