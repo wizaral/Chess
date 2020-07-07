a4c6b94004bc31ea991ea06a3a4a1c3eaa248752
wizaral <alex.macromaster@outlook.com>
2020-07-07 14:41:18 +0300

    player class

--- logic/inc/player.hpp
+++ logic/inc/player.hpp
@@ -1,1 +1,17 @@
 #pragma once
+
+#include <string>
+
+#include "figure.hpp"
+
+namespace Chess::Logic {
+
+class Player {
+    FigureColor color_;
+    std::string name_;
+public:
+    FigureColor color() const;
+    const std::string &name() const;
+};
+
+} // namespace Chess::Logic
