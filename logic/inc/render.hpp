4fa350e3a3da10581f83565c00acbbff9607ef6b
Oleksandr Afanasiev <oafanasiev@e1r3p5.unit.ua>
2020-07-09 22:14:34 +0300

    add end game output info

--- logic/inc/render.hpp
+++ logic/inc/render.hpp
@@ -7,7 +7,8 @@ class Render {
 public:
     virtual void show_menu() = 0;
     virtual void show_board(const Logic::Board &board) = 0;
-    virtual void show_error() = 0;
+    virtual void show_error(Logic::GameState state) = 0;
+    virtual void show_endgame(Logic::GameState state, Logic::Player *winer) = 0;
 };
 
 } // namespace Chess
