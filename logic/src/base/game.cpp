bb31737cc240a6d4fb1faa48b12a7f9290b38883
wizaral <alex.macromaster@outlook.com>
2020-07-16 16:44:31 +0300

    fix polymorphism unique ptr using

--- logic/src/base/game.cpp
+++ logic/src/base/game.cpp
@@ -13,22 +13,22 @@ ChessGame::ChessGame(std::array<std::unique_ptr<Logic::Player>, 2> arr,
 }
 
 void ChessGame::init_game() {
-    Logic::FigureColor player0_color = players_[0]->color(), p1c = players_[1]->color();
+    Logic::FigureColor p0c = players_[0]->color(), p1c = players_[1]->color();
 
-    if (player0_color == Logic::FigureColor::Light) {
-        board_.add_figure({Logic::FigureType::Queen, Logic::FigureColor::Light, std::make_unique<Logic::Strategy>(Logic::QueenStrategy())}, {Logic::player0_figures_row, 3});
-        board_.add_figure({Logic::FigureType::King, Logic::FigureColor::Light, std::make_unique<Logic::Strategy>(Logic::KingStrategy())}, {Logic::player0_figures_row, 4});
-        board_.add_figure({Logic::FigureType::Queen, Logic::FigureColor::Dark, std::make_unique<Logic::Strategy>(Logic::QueenStrategy())}, {Logic::player1_figures_row, 3});
-        board_.add_figure({Logic::FigureType::King, Logic::FigureColor::Dark, std::make_unique<Logic::Strategy>(Logic::KingStrategy())}, {Logic::player1_figures_row, 4});
+    if (p0c == Logic::FigureColor::Light) {
+        board_.add_figure({Logic::FigureType::Queen, Logic::FigureColor::Light, std::make_unique<Logic::QueenStrategy>()}, {Logic::player0_figures_row, 3});
+        board_.add_figure({Logic::FigureType::King, Logic::FigureColor::Light, std::make_unique<Logic::KingStrategy>()}, {Logic::player0_figures_row, 4});
+        board_.add_figure({Logic::FigureType::Queen, Logic::FigureColor::Dark, std::make_unique<Logic::QueenStrategy>()}, {Logic::player1_figures_row, 3});
+        board_.add_figure({Logic::FigureType::King, Logic::FigureColor::Dark, std::make_unique<Logic::KingStrategy>()}, {Logic::player1_figures_row, 4});
     } else {
-        board_.add_figure({Logic::FigureType::Queen, Logic::FigureColor::Dark, std::make_unique<Logic::Strategy>(Logic::QueenStrategy())}, {Logic::player0_figures_row, 4});
-        board_.add_figure({Logic::FigureType::King, Logic::FigureColor::Dark, std::make_unique<Logic::Strategy>(Logic::KingStrategy())}, {Logic::player0_figures_row, 3});
-        board_.add_figure({Logic::FigureType::Queen, Logic::FigureColor::Light, std::make_unique<Logic::Strategy>(Logic::QueenStrategy())}, {Logic::player1_figures_row, 4});
-        board_.add_figure({Logic::FigureType::King, Logic::FigureColor::Light, std::make_unique<Logic::Strategy>(Logic::KingStrategy())}, {Logic::player1_figures_row, 3});
+        board_.add_figure({Logic::FigureType::Queen, Logic::FigureColor::Dark, std::make_unique<Logic::QueenStrategy>()}, {Logic::player0_figures_row, 4});
+        board_.add_figure({Logic::FigureType::King, Logic::FigureColor::Dark, std::make_unique<Logic::KingStrategy>()}, {Logic::player0_figures_row, 3});
+        board_.add_figure({Logic::FigureType::Queen, Logic::FigureColor::Light, std::make_unique<Logic::QueenStrategy>()}, {Logic::player1_figures_row, 4});
+        board_.add_figure({Logic::FigureType::King, Logic::FigureColor::Light, std::make_unique<Logic::KingStrategy>()}, {Logic::player1_figures_row, 3});
     }
 
-    spawn_figures(Logic::player0_figures_row, player0_color);
-    spawn_pawns(Logic::player0_pawns_row, player0_color, Logic::player0_step_direction);
+    spawn_figures(Logic::player0_figures_row, p0c);
+    spawn_pawns(Logic::player0_pawns_row, p0c, Logic::player0_step_direction);
 
     spawn_figures(Logic::player1_figures_row, p1c);
     spawn_pawns(Logic::player1_pawns_row, p1c, Logic::player1_step_direction);
@@ -41,20 +41,20 @@ void ChessGame::validate() const {
 
 void ChessGame::spawn_pawns(int row, Logic::FigureColor color, int direction) {
     for (int i = 0; i < Logic::board_rows; ++i) {
-        board_.add_figure({Logic::FigureType::Pawn, color, std::make_unique<Logic::Strategy>(Logic::PawnStrategy(this, direction))}, {row, i});
+        board_.add_figure({Logic::FigureType::Pawn, color, std::make_unique<Logic::PawnStrategy>(this, direction)}, {row, i});
         subscribe(static_cast<Logic::PawnStrategy *>(board_.get_figure({row, i})->strategy()));
     }
 }
 
 void ChessGame::spawn_figures(int row, Logic::FigureColor color) {
-    board_.add_figure({Logic::FigureType::Rook, color, std::make_unique<Logic::Strategy>(Logic::RookStrategy())}, {row, 0});
-    board_.add_figure({Logic::FigureType::Rook, color, std::make_unique<Logic::Strategy>(Logic::RookStrategy())}, {row, 7});
+    board_.add_figure({Logic::FigureType::Rook, color, std::make_unique<Logic::RookStrategy>()}, {row, 0});
+    board_.add_figure({Logic::FigureType::Rook, color, std::make_unique<Logic::RookStrategy>()}, {row, 7});
 
-    board_.add_figure({Logic::FigureType::Knight, color, std::make_unique<Logic::Strategy>(Logic::KnightStrategy())}, {row, 1});
-    board_.add_figure({Logic::FigureType::Knight, color, std::make_unique<Logic::Strategy>(Logic::KnightStrategy())}, {row, 6});
+    board_.add_figure({Logic::FigureType::Knight, color, std::make_unique<Logic::KnightStrategy>()}, {row, 1});
+    board_.add_figure({Logic::FigureType::Knight, color, std::make_unique<Logic::KnightStrategy>()}, {row, 6});
 
-    board_.add_figure({Logic::FigureType::Bishop, color, std::make_unique<Logic::Strategy>(Logic::BishopStrategy())}, {row, 2});
-    board_.add_figure({Logic::FigureType::Bishop, color, std::make_unique<Logic::Strategy>(Logic::BishopStrategy())}, {row, 5});
+    board_.add_figure({Logic::FigureType::Bishop, color, std::make_unique<Logic::BishopStrategy>()}, {row, 2});
+    board_.add_figure({Logic::FigureType::Bishop, color, std::make_unique<Logic::BishopStrategy>()}, {row, 5});
 }
 
 void ChessGame::transform_pawn(Logic::Position pos) {
@@ -64,16 +64,18 @@ void ChessGame::transform_pawn(Logic::Position pos) {
 
     switch (ft) {
     case Logic::FigureType::Queen:
-        board_.add_figure({ft, fc, std::make_unique<Logic::Strategy>(Logic::QueenStrategy())}, pos);
+        board_.add_figure({ft, fc, std::make_unique<Logic::QueenStrategy>()}, pos);
         break;
     case Logic::FigureType::Rook:
-        board_.add_figure({ft, fc, std::make_unique<Logic::Strategy>(Logic::RookStrategy())}, pos);
+        board_.add_figure({ft, fc, std::make_unique<Logic::RookStrategy>()}, pos);
         break;
     case Logic::FigureType::Knight:
-        board_.add_figure({ft, fc, std::make_unique<Logic::Strategy>(Logic::KnightStrategy())}, pos);
+        board_.add_figure({ft, fc, std::make_unique<Logic::KnightStrategy>()}, pos);
         break;
     case Logic::FigureType::Bishop:
-        board_.add_figure({ft, fc, std::make_unique<Logic::Strategy>(Logic::BishopStrategy())}, pos);
+        board_.add_figure({ft, fc, std::make_unique<Logic::BishopStrategy>()}, pos);
+        break;
+    default:
         break;
     }
 }
@@ -84,6 +86,7 @@ void ChessGame::loop() {
     for (; !is_endgame(gstate); ++player_index_) {
         render_->show_board(board_);
 
+        render_->show_next_step(get_current_player());
         Logic::Move m = input_->get_next_move(get_current_player());
         gstate = logic(m);
 
