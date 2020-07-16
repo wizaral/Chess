bbd990bf7a6336a760d2b161c5ee49d9e8133c4e
wizaral <alex.macromaster@outlook.com>
2020-07-16 16:47:29 +0300

    add standard chess input (e2e4)

--- view/src/console/input.cpp
+++ view/src/console/input.cpp
@@ -1,13 +1,38 @@
+#include <iostream>
+
 #include "c_input.hpp"
 
 Chess::Logic::Move ConsoleInput::get_next_move(Chess::Logic::Player *player) {
-    return {{0, 1}, {0, 0}};
+    std::string input;
+
+    // std::cin.ignore();
+    std::cin >> input;
+
+    char col_from = input[0], col_to = input[2];
+    int row_from = input[1], row_to = input[3];
+
+    // std::cout << input << ": " << col_from << row_from << "-" << col_to << row_to << std::endl;
+
+    return {transform(row_from, col_from), transform(row_to, col_to)};
 }
 
 Chess::Logic::FigureType ConsoleInput::promote_figure(Chess::Logic::Player *player, Chess::Logic::Position pos) {
-    return Chess::Logic::FigureType::Pawn;
+    char type;
+    // std::cin.ignore();
+    std::cin >> type;
+
+    switch (type) {
+    case 'B':
+        return Chess::Logic::FigureType::Bishop;
+    case 'N':
+        return Chess::Logic::FigureType::Knight;
+    case 'R':
+        return Chess::Logic::FigureType::Rook;
+    default:
+        return Chess::Logic::FigureType::Queen;
+    }
 }
 
 Chess::Logic::Position ConsoleInput::transform(int row, char col) {
-    return {0, 1};
+    return {56 - row, (int)col - 97};
 }
