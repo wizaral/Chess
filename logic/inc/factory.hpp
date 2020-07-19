76b821663800e58f3a145a59f28dda7df1ddb114
wizaral <alex.macromaster@outlook.com>
2020-07-19 19:57:01 +0300

    make players array constant

--- logic/inc/factory.hpp
+++ logic/inc/factory.hpp
@@ -8,12 +8,12 @@ namespace Chess {
 class FigureFactory {
 public:
     virtual ~FigureFactory() = default;
-    virtual void init_game(Logic::Board &board, int &player_index, std::array<std::unique_ptr<Logic::Player>, 2> &players, Publisher *publisher) = 0;
+    virtual void init_game(Logic::Board &board, int &player_index, const std::array<std::unique_ptr<Logic::Player>, 2> &players, Publisher *publisher) = 0;
 };
 
 class ClassicFactory final : public FigureFactory {
 public:
-    void init_game(Logic::Board &board, int &player_index, std::array<std::unique_ptr<Logic::Player>, 2> &players, Publisher *publisher) override;
+    void init_game(Logic::Board &board, int &player_index, const std::array<std::unique_ptr<Logic::Player>, 2> &players, Publisher *publisher) override;
 private:
     void spawn_pawns(Logic::Board &board, int row, Logic::FigureColor color, int direction, Publisher *publisher);
     void spawn_figures(Logic::Board &board, int row, Logic::FigureColor color);
