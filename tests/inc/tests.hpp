2b796bbadd1695431393a4f6eed876ebadc32639
wizaral <alexafa@ukr.net>
2020-08-01 15:37:34 +0300

    create test class

--- tests/inc/tests.hpp
+++ tests/inc/tests.hpp
@@ -1,2 +1,48 @@
 #pragma once
+#include "t_render.hpp"
+#include "t_input.hpp"
 #include "file_factory.hpp"
+
+class TestGame : public Chess::ChessGame {
+public:
+    template<class T>
+    using Field = std::array<std::array<T, 8>, 8>;
+
+    TestGame(std::array<std::unique_ptr<Chess::Logic::Player>, 2> arr,
+        std::unique_ptr<Chess::FigureFactory> factory,
+        std::unique_ptr<Chess::Render> render,
+        std::unique_ptr<Chess::DataInput> input)
+        : Chess::ChessGame(std::move(arr)
+        , std::move(factory)
+        , std::move(render)
+        , std::move(input)) {}
+
+    Chess::Logic::GameState logic(const Chess::Logic::Move &move) {
+        return Chess::ChessGame::logic(move);
+    }
+
+    const Field<std::unique_ptr<Chess::Logic::Figure>> &figures() const {
+        return board_.figures();
+    }
+
+    const Chess::Logic::Board &board() const {
+        return board_;
+    }
+
+    Chess::Render *render() {
+        return render_.get();
+    }
+};
+
+inline TestGame init(const std::string &file_name) {
+    return TestGame({
+        std::make_unique<Chess::Logic::Player>(Chess::Logic::FigureColor::White, "white"),
+        std::make_unique<Chess::Logic::Player>(Chess::Logic::FigureColor::Black, "black")},
+        std::make_unique<FileFactory>(file_name),
+        std::make_unique<TestRender>(),
+        std::make_unique<TestInput>());
+}
+
+inline std::ostream &operator<<(std::ostream &os, const std::unique_ptr<Chess::Logic::Figure> &figure) {
+    return os << TestRender::colors.at(figure->color()) << TestRender::figures.at(figure->type()) << ' ';
+}
---
+++ tests/src/tests.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "c_input.hpp"
+#include "c_render.hpp"
+#include "tests.hpp"
+#include "test_runner.hpp"
+
+void test0() {
+    TestGame game = init(".\\tests\\tests\\1.txt");
+    game.init_game();
+    game.render()->show_board(game.board());
+
+    std::cout << "D1" << std::endl;
+    std::array<std::array<std::unique_ptr<Chess::Logic::Figure>, 8>, 8> correct;
+    correct[4][4] = std::make_unique<Chess::Logic::Figure>(Chess::Logic::FigureType::Pawn, Chess::Logic::FigureColor::White, std::make_unique<Chess::Logic::PawnStrategy>(nullptr, Chess::Logic::FigureColor::White, Chess::Logic::PawnStrategy::MoveState::NormalMove));
+    std::cout << "D1" << std::endl;
+
+    Chess::Logic::Move move{TestInput::transform(2, 'e'), TestInput::transform(4, 'e')};
+    Chess::Logic::GameState gstate = game.logic(move);
+
+    ASSERT_EQUAL(gstate, Chess::Logic::GameState::NormalMove);
+    ASSERT_EQUAL(game.figures(), correct);
+}
+
+int main() {
+    // TestGame game({
+    //     std::make_unique<Chess::Logic::Player>(Chess::Logic::FigureColor::White, "white"),
+    //     std::make_unique<Chess::Logic::Player>(Chess::Logic::FigureColor::Black, "black"),
+    //     }, std::make_unique<FileFactory>(), std::make_unique<ConsoleRender>(), std::make_unique<ConsoleInput>());
+
+    // game.init_game();
+    TestRunner tr(false);
+
+    RUN_TEST(tr, test0);
+    // RUN_TEST(tr, test1);
+}
