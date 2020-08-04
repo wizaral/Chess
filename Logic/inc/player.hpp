8110f35583a8975c8a22db42b63dad4f0da7376e
wizaral <alexafa@ukr.net>
2020-08-04 14:48:19 +0300

    add virtual dctor and abstract methods to player

--- Logic/inc/player.hpp
+++ Logic/inc/player.hpp
@@ -2,6 +2,7 @@
 #include <string>
 
 #include "figure.hpp"
+#include "move.hpp"
 
 namespace Chess::Logic {
 
@@ -11,9 +12,13 @@ class Player {
 
 public:
     Player(FigureColor color, std::string name);
+    virtual ~Player() = default;
 
     FigureColor color() const;
     const std::string &name() const;
+
+    virtual Move get_next_move() = 0;
+    virtual FigureType promote_figure(const Chess::Logic::Position &pos) = 0;
 };
 
 } // namespace Chess::Logic
