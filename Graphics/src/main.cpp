0fc0992614006da00cec8438c8b2b13b09c8d4dd
wizaral <alexafa@ukr.net>
2020-08-05 05:22:10 +0300

    update main function

--- Graphics/src/main.cpp
+++ Graphics/src/main.cpp
@@ -1,33 +1,10 @@
-#include "g_input.hpp"
-#include "g_render.hpp"
-#include "g_player.hpp"
-#include "chess.hpp"
-
-void after_game(sf::RenderWindow &window) {
-    for (sf::Event event; window.isOpen() && window.waitEvent(event);) {
-        if (event.type == sf::Event::Closed || event.type == sf::Event::KeyPressed) {
-            window.close();
-        }
-    }
-}
+#include "game.hpp"
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(window_width, window_height), "Chess", sf::Style::Titlebar | sf::Style::Close);
-    const auto& mode = sf::VideoMode::getDesktopMode();
-    window.setPosition(sf::Vector2i{(static_cast<int>(mode.width) - window_width) / 2, 0});
+    window.setPosition({(static_cast<int>(sf::VideoMode::getDesktopMode().width) - window_width) / 2, 0});
+    window.setFramerateLimit(60);
 
-    Chess::ChessGame game({
-        std::make_unique<RealPlayer>(Chess::Logic::FigureColor::White, "white", window),
-        std::make_unique<RealPlayer>(Chess::Logic::FigureColor::Black, "black", window),
-        },
-        std::make_unique<Chess::ClassicFactory>(),
-        std::make_unique<GraphicsRender>(window),
-        std::make_unique<GraphicsInput>(),
-        [&window]() -> bool {
-            return window.isOpen();
-        });
-
-    game.init_game();
-    game.loop();
-    after_game(window);
+    Game game(window);
+    game.menu();
 }
