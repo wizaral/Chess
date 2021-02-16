55f9e53a0e8f75a308f5f269cbc71255591d1381
wizaral <alexafa@ukr.net>
2021-02-16 19:05:06 +0200

    change enums values

--- logic/figure.hpp
+++ logic/figure.hpp
@@ -2,21 +2,18 @@
 
 namespace Chess {
 
-#define FigureTypeMask 0x3F
-#define FigureColorMask 0xC0
-
 enum class FigureType : uint8_t {
-    Pawn = 1 << 0,
-    Knight = 1 << 1,
-    Bishop = 1 << 2,
-    Rook = 1 << 3,
-    Queen = 1 << 4,
-    King = 1 << 5,
+    Pawn,
+    Knight,
+    Bishop,
+    Rook,
+    Queen,
+    King,
 };
 
 enum class FigureColor : uint8_t {
-    White = 1 << 6,
-    Black = 1 << 7,
+    White,
+    Black,
 };
 
 FigureColor operator!(FigureColor color);
