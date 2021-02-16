3d5369be41461a277de935256c82a353a7037d0c
wizaral <alexafa@ukr.net>
2021-02-16 19:18:31 +0200

    update factory methods, clean code, add includes

--- logic/factory.cpp
+++ logic/factory.cpp
@@ -1,6 +1,11 @@
 #include "definitions.hpp"
 #include "factory.hpp"
-#include "strategy.hpp"
+#include "figure_bishop.hpp"
+#include "figure_king.hpp"
+#include "figure_knight.hpp"
+#include "figure_pawn.hpp"
+#include "figure_queen.hpp"
+#include "figure_rook.hpp"
 
 namespace Chess {
 
@@ -14,28 +19,28 @@ void ClassicFactory::init_game(Board &board, int &player_index, Publisher *publi
     board.reset_check_state(FigureColor::White);
     board.reset_check_state(FigureColor::Black);
 
-    spawn_figures(board, white_figures_row, FigureColor::White);
-    spawn_figures(board, black_figures_row, FigureColor::Black);
+    spawn_figures(board, figures_row[static_cast<int>(FigureColor::White)], FigureColor::White);
+    spawn_figures(board, figures_row[static_cast<int>(FigureColor::Black)], FigureColor::Black);
 
-    spawn_pawns(board, white_pawns_row, FigureColor::White, publisher);
-    spawn_pawns(board, black_pawns_row, FigureColor::Black, publisher);
+    spawn_pawns(board, pawns_row[static_cast<int>(FigureColor::White)], FigureColor::White, publisher);
+    spawn_pawns(board, pawns_row[static_cast<int>(FigureColor::Black)], FigureColor::Black, publisher);
 }
 
 void ClassicFactory::spawn_pawns(Board &board, int row, FigureColor color, Publisher *publisher) {
     for (int i = 0; i < board_rows; ++i) {
-        board.add_figure({FigureType::Pawn, color, std::make_unique<PawnStrategy>(publisher, color)}, {row, i});
+        board.add_figure(std::make_unique<Pawn>(FigureType::Pawn, color, publisher), {row, i});
     }
 }
 
 void ClassicFactory::spawn_figures(Board &board, int row, FigureColor color) {
-    board.add_figure({FigureType::Rook, color, std::make_unique<RookStrategy>()}, {row, 0});
-    board.add_figure({FigureType::Knight, color, std::make_unique<KnightStrategy>()}, {row, 1});
-    board.add_figure({FigureType::Bishop, color, std::make_unique<BishopStrategy>()}, {row, 2});
-    board.add_figure({FigureType::Queen, color, std::make_unique<QueenStrategy>()}, {row, 3});
-    board.add_figure({FigureType::King, color, std::make_unique<KingStrategy>()}, {row, 4});
-    board.add_figure({FigureType::Bishop, color, std::make_unique<BishopStrategy>()}, {row, 5});
-    board.add_figure({FigureType::Knight, color, std::make_unique<KnightStrategy>()}, {row, 6});
-    board.add_figure({FigureType::Rook, color, std::make_unique<RookStrategy>()}, {row, 7});
+    board.add_figure(std::make_unique<Rook>(FigureType::Rook, color), {row, 0});
+    board.add_figure(std::make_unique<Knight>(FigureType::Knight, color), {row, 1});
+    board.add_figure(std::make_unique<Bishop>(FigureType::Bishop, color), {row, 2});
+    board.add_figure(std::make_unique<Queen>(FigureType::Queen, color), {row, 3});
+    board.add_figure(std::make_unique<King>(FigureType::King, color), {row, 4});
+    board.add_figure(std::make_unique<Bishop>(FigureType::Bishop, color), {row, 5});
+    board.add_figure(std::make_unique<Knight>(FigureType::Knight, color), {row, 6});
+    board.add_figure(std::make_unique<Rook>(FigureType::Rook, color), {row, 7});
 }
 
 } // namespace Chess
