cfc932a933944886dde05bedf08156b239940fc4
wizaral <alexafa@ukr.net>
2021-02-16 18:59:52 +0200

    pack global vars to arrays

--- logic/definitions.hpp
+++ logic/definitions.hpp
@@ -1,17 +1,24 @@
 #pragma once
+#include <array>
 
 namespace Chess {
 
-inline constexpr int board_rows = 8;
-inline constexpr int board_cols = 8;
+inline static constexpr int board_rows = 8;
+inline static constexpr int board_cols = 8;
 
-inline constexpr int white_pawns_row = 1;
-inline constexpr int black_pawns_row = 6;
+inline static constexpr std::array<int, 2> pawns_row{
+    1,
+    6,
+};
 
-inline constexpr int white_figures_row = 0;
-inline constexpr int black_figures_row = 7;
+inline static constexpr std::array<int, 2> figures_row{
+    0,
+    7,
+};
 
-inline constexpr int white_step_direction = 1;
-inline constexpr int black_step_direction = -1;
+inline static constexpr std::array<int, 2> step_direction{
+    1,
+    -1,
+};
 
 } // namespace Chess
